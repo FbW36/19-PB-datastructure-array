52712f3130ac1804a9c5b9fb8b27442d7fec5921
Cindy <Punk.c@hotmail.com>
2020-10-20 17:58:12 +0200

    adding amazing code

--- hallo.js
+++ hallo.js
@@ -1,7 +1,16 @@
-const myArr = ["item1", "item2", ["subSubItem1", "subSubItem2"]];
-myArr[1] = 'Spain'
+const myArr = [ 'item1', 'item2', ['subSubItem1', 'subSubItem2' ] ]
+
+
+//Using push (end of array)
+myArr.push("Germany")
+//Using unshift (start of array)
+myArr.unshift("Spain")
+//Editing the array
 myArr[2] = 'SET item2'
-myArr[3][0] = "Berlin"
+//Adding Berlin and fixing the second array
+myArr[3][0]= 'Berlin'
 myArr[3][1]= 'subSubItem1'
-myArr.push("Germany");
-myArr.push('Spain', 'item1', 'SET item2', ['Berlin','subSubItem1' ], 'Germany');
+
+
+//getting the best answers :v 
+console.log(myArr)
