df6d2a1364a47cde5c333ccb160f3250290e59ca
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-10-20 15:19:17 +0200

    Adding other options to the solution

--- adrian/script.js
+++ adrian/script.js
@@ -7,3 +7,12 @@ myArr[3][0] = 'Berlin';
 myArr[3][1] = 'subSubItem1';
 myArr.push('Germany');
 console.log(myArr);
+
+// WITH POP AND PUSH ONLY
+/* myArr.pop();
+myArr[0] = "Spain";
+myArr[1] = "item1";
+myArr.push("SET item2");
+myArr.push(["Berlin", "subSubItem1"]);
+myArr.push("Germany");
+console.log(myArr); */
